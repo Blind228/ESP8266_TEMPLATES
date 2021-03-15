6ac377f33517ff147617ba29e7c8ac6654a3967e
Blind228 <37125951+Blind228@users.noreply.github.com>
2021-03-15 17:06:09 -0400

    updated version

--- ESP8266_OTA-MQTT_TEMPLATE/ESP8266_OTA-MQTT_TEMPLATE.ino
+++ ESP8266_OTA-MQTT_TEMPLATE/ESP8266_OTA-MQTT_TEMPLATE.ino
@@ -1,27 +1,28 @@
-/* 
-
-
+/*
+  Wemos D1 Mini code template for MQTT and OTA projects.
 */
-
+#include "secrets.h"  //For Credentials
 #include <ESP8266WiFi.h>  //For ESP8266
 #include <PubSubClient.h> //For MQTT
 #include <ESP8266mDNS.h>  //For OTA
 #include <WiFiUdp.h>      //For OTA
 #include <ArduinoOTA.h>   //For OTA
 
-//WIFI configuration
-#define wifi_ssid "wifi_ssid"
-#define wifi_password "wifi_password"
+// Update the "secrets.h" file with values suitable for your network.
+// WIFI
+const char* wifi_ssid = SECRET_WIFI_SSID;
+const char* wifi_passwd = SECRET_WIFI_PASSWD;
+// MQTT
+const char* mqtt_server = SECRET_MQTT_SERVER;
+const char* mqtt_user = SECRET_MQTT_USER;
+const char* mqtt_passwd = SECRET_MQTT_PASSWD;
 
-// OTA configuration
-#define ota_password "ota_password"
+const char* mqtt_client_id = "ESP8266-"; // ClientID on MQTT
+const char* outTopic = "/confirmation"; // Confirmation Topic
+const char* inTopic = "/test";  // Choose topic to subscribe to.
 
-//MQTT configuration
-#define mqtt_server "255.255.255.255" // Internal IP adress of your MQTT broker.
-#define mqtt_user "mqtt_user"        // If you don't want to use a username and password for MQTT see the
-#define mqtt_password "mqtt_password"  // comments in the "void mqtt_reconnect()" function lower in the code.
-#define use_chip_id true // Set to false to remove chip ID from "mqtt_client_id".
-String mqtt_client_id = "ESP8266-"; //This text is concatenated with ChipId if previous line set to true.
+// OTA
+const char* ota_passwd = SECRET_OTA_PASSWD;
 
 // MQTT client
 WiFiClient espClient;
@@ -30,43 +31,148 @@ PubSubClient mqtt_client(espClient);
 // Necesary to make Arduino Software autodetect OTA device
 WiFiServer TelnetServer(8266);
 
+//Variables Definition
+//Built-in LED on Wemos D1 Mini is GPIO2 (D4)
+// const int ledPin = 4;
+// const int relayPin = 4;
+
+
 // USER DEFINED SETUP
-void setup_variables() {    // ****************INSERT SETUP COMMANDS HERE********************
-  
+void user_setup() {   // ****************INSERT SETUP COMMANDS HERE********************
+  // Use this function to define pins and other setup commands.
   pinMode(LED_BUILTIN, OUTPUT); // Initialise builtin LED.
-  
+  digitalWrite(LED_BUILTIN, HIGH);   // Turn the LED off by making the voltage HIGH
+
 }
 
 // USER DEFINED LOGIC
-void setup_loop() {         // ****************INSERT LOOP COMMANDS HERE*********************
-  
+void user_loop() {    // ****************INSERT LOOP COMMANDS HERE*********************
+
   // Blink test
   digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW turns the LED on for the ESP-8266)
   delay(1000);                  // Wait for 1 seconds
   digitalWrite(LED_BUILTIN, HIGH);   // Turn the LED off by making the voltage HIGH
   delay(5000);                  // Wait for 5 seconds
 
-  // MQTT comunications
-  // mqtt_client.publish ("/topic", "payload"); 
+  // MQTT comunications HERE
+  // mqtt_client.publish ("/topic", "payload");
+  // ANY LOGIC ACTIVATED BY RECEIVED MQTT PAYLOADS FROM SUBSCRIBED
+  // TOPICS MUST BE IMPLEMENTED IN TH CALLBACK FUNCTION.(SEE BELOW)
+}
+
+// Callback function for MQTT. This is used to receive messages
+// on subscribed topics. Not needed if not subsrcibed to any topics.
+void callback(char* topic, byte* payload, unsigned int length) {
+  // Conver the incoming byte array to a string
+  payload[length] = '\0'; // Null terminator used to terminate the char array
+  String message = (char*)payload;
+
+  Serial.print("Message arrived on topic: [");
+  Serial.print(topic);
+  Serial.print("], ");
+  Serial.println(message);
+
+  if (message == "test") {  // Define what message is the trigger.
+    mqtt_client.publish(outTopic, "MQTT message received!");
+  }
+}
+
+// ---------Past this point no modification is needed.--------------
+
+// INITIAL SETUP.
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(10); // wait for serial port to connect.
+  }
+  Serial.println("\r\nBooting...");
+
+  setup_wifi(); // Connect to WIFI.
+
+  ota_setup();  // Setup OTA.
+
+  mqtt_setup(); // Setup MQTT.
+
+  user_setup(); // User setup.
+
+  Serial.println("Setup completed! Running app...");
 }
 
 
-// INITIAL SETUP LOOP.
-void setup() {
-  Serial.begin(9600);
-  while (!Serial) {delay(10);} // wait for serial port to connect.
-  Serial.println("\r\nBooting...");
+// MAIN LOOP.
+void loop() {
 
-  setup_wifi(); // Connect to wifi.
+  ArduinoOTA.handle();
 
-  setup_variables(); // User function.
+  if (!mqtt_client.connected()) {
+    mqtt_reconnect(); // If MQTT connection is lost, reconnect.
+  }
 
+  mqtt_client.loop();
+
+  user_loop();
+}
+
+
+// ------------FUNCTIONS------------------------
+
+// WIFI SETUP FUNCTION.
+void setup_wifi() {
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.begin(wifi_ssid, wifi_passwd);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+}
+
+// MQTT SETUP FUNCTION.
+void mqtt_setup() {
+  Serial.println("Configuring MQTT server...");
+
+  mqtt_client.setServer(mqtt_server, 1883);
+  mqtt_client.setCallback(callback);
+  Serial.printf("   Server IP: %s\r\n", mqtt_server);
+  Serial.printf("   Username:  %s\r\n", mqtt_user);
+  Serial.printf("   Cliend Id: %s\r\n", mqtt_client_id);
+  Serial.println("   MQTT setup completed.");
+}
+
+
+// MQTT RECONNECTION FUNCTION.
+void mqtt_reconnect() {
+  while (!mqtt_client.connected()) {  // Loop until we're reconnected
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    // If you do not want to use a username and password, change next line to
+    // if (client.connect("ESP8266Client")) {
+    if (mqtt_client.connect(mqtt_client_id, mqtt_user, mqtt_passwd)) {
+      mqtt_client.publish(outTopic, mqtt_client_id);  // Publish ID to show we're connected.
+      mqtt_client.subscribe(inTopic); // Re-Subscribe.
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+// OTA SETUP FUNCTION.
+void ota_setup() {
   Serial.print("Configuring OTA device...");
   TelnetServer.begin();   //Necesary to make Arduino Software autodetect OTA device
   ArduinoOTA.onStart([]() {
     Serial.println("OTA starting...");
   });
-  ArduinoOTA.setPassword(ota_password);
+  ArduinoOTA.setPassword(ota_passwd);
   ArduinoOTA.onEnd([]() {
     Serial.println("OTA update finished!");
     Serial.println("Rebooting...");
@@ -83,71 +189,5 @@ void setup() {
     else if (error == OTA_END_ERROR) Serial.println("End Failed");
   });
   ArduinoOTA.begin();
-  Serial.println("OK");
-
-  Serial.println("Configuring MQTT server...");
-
-if (use_chip_id == true) {
-   mqtt_client_id = mqtt_client_id + ESP.getChipId();
-}
-else {
-  mqtt_client_id = mqtt_client_id;
-}
-  
-  mqtt_client.setServer(mqtt_server, 1883);
-  Serial.printf("   Server IP: %s\r\n", mqtt_server);
-  Serial.printf("   Username:  %s\r\n", mqtt_user);
-  Serial.println("   Cliend Id: " + mqtt_client_id);
-  Serial.println("   MQTT configured!");
-
-  Serial.println("Setup completed! Running app...");
-}
-
-// MAIN LOOP.
-void loop() {
-
-  ArduinoOTA.handle();
-
-  if (!mqtt_client.connected()) {
-    mqtt_reconnect();
-  }
-  mqtt_client.loop();
-  
-  setup_loop();
-}
-
-
-
-// SETUP_WIFI FUNCTION.
-void setup_wifi() {
-  delay(10);
-  Serial.print("Connecting to ");
-  Serial.print(wifi_ssid);
-  WiFi.begin(wifi_ssid, wifi_password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("OK");
-  Serial.print("   IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-// MQTT RECONNECTION FUNCTION.
-void mqtt_reconnect() {
-  while (!mqtt_client.connected()) {  // Loop until we're reconnected
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    // If you do not want to use a username and password, change next line to
-    // if (client.connect("ESP8266Client")) {
-    if (mqtt_client.connect(mqtt_client_id.c_str(), mqtt_user, mqtt_password)) {
-      Serial.println("connected");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(mqtt_client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
+  Serial.println("   OTA setup completed.");
 }
